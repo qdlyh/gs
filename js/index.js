e1b548789a01ebdd12b36f69fafb297e98ed5f31
qdlyh <liang620523@163.com>
2017-05-12 10:30:12 +0800

    message

---
+++ js/index.js
@@ -0,0 +1,8 @@
+       $(function(){
+	    	$('.head-nav').mouseover(function(){
+	    		$(this).children('ul').show()
+	    	})
+	    	$('.head-nav').mouseout(function(){
+	    		$(this).children('ul').hide()
+	    	})
+  })
